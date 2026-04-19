47ba92ebb10c4d2700224a8e5ec262937045f261
Ayoub Aguezar <ayoubaguezzar1@gmail.com>
2026-04-19 23:15:32 +0100

    Delete convert2.cjs

--- convert2.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const b64 = fs.readFileSync('public/signature.png').toString('base64');
-const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="150" viewBox="0 0 300 150">
-  <image href="data:image/png;base64,${b64}" width="100%" height="100%" preserveAspectRatio="xMidYMid meet"/>
-</svg>`;
-fs.writeFileSync('public/signature.svg', svg);
