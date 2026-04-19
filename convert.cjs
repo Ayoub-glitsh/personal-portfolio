b478973ad3674e8b7e99b619749e35ea17a8b3ae
Ayoub Aguezar <ayoubaguezzar1@gmail.com>
2026-04-19 23:14:18 +0100

    Delete convert.cjs

--- convert.cjs
+++
@@ -1,8 +0,0 @@
-const ImageTracer = require('imagetracerjs');
-const fs = require('fs');
-
-ImageTracer.imageToSVG(
-    'public/signature.png',
-    function(svgstr){ fs.writeFileSync( 'public/signature.svg', svgstr ); },
-    { ltres:0.1, qtres:1, pathomit:8, colorsampling:1, numberofcolors:2, mincolorratio:0, colorquantcycles:3 }
-);
